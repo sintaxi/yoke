ce54bcc052635b312804a486d378504088bb17eb
Brock Whitten <brock@sintaxi.com>
2014-01-10 03:34:28 -0800

    passing test

--- yoke.js
+++ yoke.js
@@ -2,5 +2,26 @@ var fs    = require("fs")
 var path  = require("path")
 
 module.exports = function(sourcePath, callback){
-  callback(null, sourcePath)
+  var sourceAbsolutePath = path.resolve(sourcePath)
+  fs.readFile(sourceAbsolutePath, function(err, content){
+    if(err) return callback(err)
+
+    var files     = content.toString().split("\n")
+    var total     = files.length
+    var count     = 0
+    var contents  = []
+    var sourceDir = path.dirname(sourceAbsolutePath)
+    var body      = []
+
+    files.forEach(function(item){
+      var childPath = path.join(sourceDir, item)
+      fs.readFile(childPath, function(err, cont){
+        count ++
+        // TODO: order this
+        body.push(cont.toString())
+        if(count === total) callback(null, body.join("\n"))
+      })
+    })
+
+  })
 }
